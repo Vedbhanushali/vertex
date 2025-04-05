6f41636918e94a584177f040839218aa9783ce4f
Vedbhanushali <19ce005@charusat.edu.in>
2025-04-05 18:40:52 +0530

    Shadcn setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,9 +9,9 @@ function App() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
